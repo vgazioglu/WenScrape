67ac0150301a63536a9968c9512014dcca6c6a42
Volki <vgazioglu@gmail.com>
2020-03-15 00:07:23 +0300

    pandas tamam, csv ye çıkıldı

--- 4.py
+++ 4.py
@@ -1,41 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
-import sys
+import pandas as pd
 
 
 def futbolcu_hareket_bilgileri(futbolcu_URL):
     URL = futbolcu_URL
     # URL = 'https://www.tff.org/Default.aspx?pageId=30&kisiId=1055923'
     page = requests.get(URL)
-
-
-
     soup = BeautifulSoup(page.content, 'html.parser')
+    # print(soup.prettify())
+    my_table2 = soup.find('table', {'class': 'yuzdeyuzH'})
+    # print(my_table2)
 
-    #print(soup.prettify())
-    #sys.exit(0)
-
-    My_table2 = soup.find('table', {'class': 'yuzdeyuzH'})
-    print(My_table2)
-    sys.exit(0)
-
-    My_table = soup.find('table', {'class': 'MasterTable_TFF_Contents'})
+    my_table = soup.find('table', {'class': 'MasterTable_TFF_Contents'})
     # print(My_table)
 
     # table = soup.find('table', border=1)
-    rows = My_table.find_all('tr')
-
-    for row in rows:
+    rows_output = my_table.find_all('tr')
+    # print(type(rows))
+    # print(rows)
+    rn_list = []
+    sr_list = []
+    dn_list = []
+    nn_list = []
+    for row in rows_output:
         data = row.find_all("td")
         if len(data) > 0:
-            rn = data[0].get_text()
+            # rn =
+            rn_list.append(data[0].get_text().strip())
+            sr_list.append(data[1].get_text().strip())
+            dn_list.append(data[2].get_text().strip())
+            nn_list.append(data[3].get_text().strip())
             sr = data[1].get_text()
             d = data[2].get_text()
             n = data[3].get_text()
-            print(str(rn).strip() + "\t" + sr + "\t" + d + "\t" + n)
+            # print(str(rn).strip() + "\t" + sr + "\t" + d + "\t" + n)
+            # print(rn_list)
+
+    weather = pd.DataFrame({
+        "KULÜP": rn_list,
+        "İLK VİZE": sr_list,
+        "SON VİZE": dn_list,
+        "DURUM": nn_list
+    })
+    print(weather)
+    weather.to_csv("futbolcu_bilgileri.csv")
+
 
 """for i in range(105933, 105944):
     futbolcu_hareket_bilgileri('https://www.tff.org/Default.aspx?pageId=30&kisiId='+str(i)+'')"""
-
 
 futbolcu_hareket_bilgileri('https://www.tff.org/Default.aspx?pageId=30&kisiId=1055923')
